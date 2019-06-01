cd5678aab9e5b00e28171e65c6b4aab62f0b5e6e
Thales Miguel <thales.miguel@gmail.com>
2019-06-01 08:56:46 -0300

    Create Identicon.hash_input/1 function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,27 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    Provides functions to create a identicon based on a string passed
+    as argument.
   """
 
   @doc """
-  Hello world.
+    Main function called for this module passing only a string argument.
+  """
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @doc """
+    Given a string input, returns a hash of 16 elements representing the
+    argument.
 
   ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,20 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  describe "hash_input/1" do
+    test "always returns the same hash based on the given string argument" do
+      string_argument = "Banana"
+      first_hash = Identicon.hash_input(string_argument)
+      second_hash = Identicon.hash_input(string_argument)
+
+      assert first_hash == second_hash
+    end
+
+    test "returns different hashes for different string arguments" do
+      first_hash = Identicon.hash_input("Banana")
+      second_hash = Identicon.hash_input("Apple")
+
+      assert first_hash != second_hash
+    end
   end
 end
